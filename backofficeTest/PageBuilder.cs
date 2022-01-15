7afd5a63d9c920c61d11736caa698ec2bd67fec1
sakul <miolynet@perfenterprise.com>
2022-01-15 18:26:55 +0700

    Refactor PageBuilder.

--- backofficeTest/PageBuilder.cs
+++ backofficeTest/PageBuilder.cs
@@ -1,56 +1,61 @@
 ﻿using Microsoft.Playwright;
+using System.Linq;
 using System.Threading.Tasks;
 
 namespace backofficeTest
 {
     public class PageBuilder
     {
-        private IBrowser browser;
         private IPlaywright playwright;
         private IBrowserContext browserContext;
         private TaskCompletionSource<IPage> loginTask;
+        private const string LaunchContextName = "backoffice";
 
         private static PageBuilder instance;
-
         public static PageBuilder Instance => instance ??= new PageBuilder();
 
-        public async Task<IPage> Build(float? slowmotion = 1000, float? loginTimeout = 60000)
-        {
-            await Initialize(slowmotion);
-            return await CreateLoginPage(loginTimeout);
-        }
-
-        protected async Task<IBrowserContext> Initialize(float? slowmotion)
+        public async Task<IPage> Build(float? slowmotion = null, bool forceLogin = false)
         {
             playwright ??= await Playwright.CreateAsync();
-            browser ??= await playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
-            {
-                Headless = false,
-                SlowMo = slowmotion,
-            });
-            return browserContext ??= await browser.NewContextAsync();
+            browserContext ??= await playwright.Chromium
+                    .LaunchPersistentContextAsync(LaunchContextName, new BrowserTypeLaunchPersistentContextOptions
+                    {
+                        Headless = false,
+                        SlowMo = slowmotion,
+                    });
+            return await CreateLoginPage(forceLogin);
         }
 
-        protected async Task<IPage> CreateLoginPage(float? loginTimeout)
+        protected async Task<IPage> CreateLoginPage(bool forceLogin)
         {
-            if (null == loginTask)
+            if (null == loginTask || forceLogin)
             {
-                loginTask = new TaskCompletionSource<IPage>();
-                var page = await browserContext.NewPageAsync();
-                await page.GotoAsync(Pages.HostUrl);
-                const string LoginButtonSelector = "text=Login";
-                await page.ClickAsync(LoginButtonSelector);
-                await page.WaitForNavigationAsync(new PageWaitForNavigationOptions
+                loginTask ??= new TaskCompletionSource<IPage>();
+                if (forceLogin || false == await hasAuthentication())
                 {
-                    Timeout = loginTimeout,
-                    UrlString = Pages.Ticket,
-                });
-                loginTask.TrySetResult(page);
-                await page.CloseAsync();
+                    await handleLoginStep();
+                }
+                loginTask.TrySetResult(null);
             }
 
             await loginTask.Task;
             return await browserContext.NewPageAsync();
+
+            async Task<bool> hasAuthentication()
+            {
+                const string SaveCookiePath = "backoffice.json";
+                var storageOptions = new BrowserContextStorageStateOptions { Path = SaveCookiePath };
+                var cookieJson = await browserContext.StorageStateAsync(storageOptions);
+                return cookieJson != "{\"cookies\":[],\"origins\":[]}" && cookieJson.Contains(".AspNetCore.OpenIdConnect");
+            }
+            async Task handleLoginStep()
+            {
+                var page = browserContext.Pages.FirstOrDefault() ?? await browserContext.NewPageAsync();
+                await page.GotoAsync(Pages.HostUrl);
+                var waitFor = new PageRunAndWaitForNavigationOptions { UrlString = Pages.Ticket };
+                await page.RunAndWaitForNavigationAsync(() => page.ClickAsync("text=Login"), waitFor);
+                await hasAuthentication();
+            }
         }
     }
 }
