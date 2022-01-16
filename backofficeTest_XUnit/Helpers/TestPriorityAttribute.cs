17761d2b1f536b8593d423c7a934de276c618154
sakul <miolynet@perfenterprise.com>
2022-01-16 16:41:32 +0700

    Add PriorityOrderer.

---
+++ backofficeTest_XUnit/Helpers/TestPriorityAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace backofficeTest_XUnit.Helpers
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+    public class TestPriorityAttribute : Attribute
+    {
+        public int Priority { get; private set; }
+
+        public TestPriorityAttribute(int priority) => Priority = priority;
+    }
+}
